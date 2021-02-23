39c716fd3961be27f0c25c0b96ecfbbd649c3085
TusharRanjan-09 <tushar.r@jeanmartin.com>
2021-02-23 13:50:37 +0530

    Updated

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useState,useEffect} from 'react';
+import { StyleSheet, View ,TextInput, TouchableOpacity, Text,} from 'react-native';
+import Index from './src/index';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Index/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical:25
+    
   },
 });
